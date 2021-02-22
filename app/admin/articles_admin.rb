f04bc4e8bc949b094f4e1121abfeae9d60ae6e8d
Tun Khine <tunkhine126@gmail.com>
2021-02-22 11:32:21 -0500

    CMS tweaks for trestle

--- app/admin/articles_admin.rb
+++ app/admin/articles_admin.rb
@@ -5,22 +5,24 @@ Trestle.resource(:articles) do
 
   # Customize the table columns shown on the index view.
   #
-  # table do
-  #   column :name
-  #   column :created_at, align: :center
-  #   actions
-  # end
+  table do
+    column :name
+    column :title
+    column :content
+    column :created_at, align: :center
+    actions
+  end
 
   # Customize the form fields shown on the new/edit views.
   #
-  # form do |article|
-  #   text_field :name
-  #
-  #   row do
-  #     col { datetime_field :updated_at }
-  #     col { datetime_field :created_at }
-  #   end
-  # end
+  form do |article|
+    text_field :name
+  
+    row do
+      col { datetime_field :updated_at }
+      col { datetime_field :created_at }
+    end
+  end
 
   # By default, all parameters passed to the update and create actions will be
   # permitted. If you do not have full trust in your users, you should explicitly
